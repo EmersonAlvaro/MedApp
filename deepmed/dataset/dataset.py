ef16341b5ebae55772370814139714379be43935
BaniaFonseca <baniafonseca@outlook.com>
2020-05-13 19:30:33 +0200

    dataset creation

--- deepmed/dataset/dataset.py
+++ deepmed/dataset/dataset.py
@@ -13,15 +13,15 @@ class DataSet:
     def __init__(self):
         x, y = self.load_dataset()
 
-        print("{}\n".format(x))
-        print("{}\n".format(y))
+        print("====== features ======= \n{} \n".format(x))
+        print("======== labels in one-hot encoding ====== \n{}\n".format(y))
        
     def load_dataset(self):
         path = Path.joinpath(data_dir,'onehotenc_symptoms_desease.csv')
         df = pd.read_csv(path)
         
-        print(df)
-        
+        print("====== entire dataset ===== \n {} ".format(df))
+
         le = LabelEncoder()
         df.desease = le.fit_transform(df.desease.values)                
         df.symptoms = [np.array(symptoms.split(' '), dtype=np.float32) for symptoms in df.symptoms]
